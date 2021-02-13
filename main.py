0bf9c113cc13f781398b82e5a7724d9170c12323
David s <dadiortiz403@gmail.com>
2021-02-12 22:10:18 -0600

    Busqueda de clientes 'for loops'

--- main.py
+++ main.py
@@ -29,6 +29,15 @@ def delete_client(client_name):
     else:
         print('Client is not in clients list')
 
+def search_client(client_name):
+    clients_list = clients.split(',')
+
+    for client in clients_list:
+        if client != client_name:
+            continue
+        else:
+            return True
+
 def _add_comma():
     global clients
 
@@ -41,7 +50,9 @@ def _print_welcome():
     print('What would you like to do today?')
     print('[C]reate client')
     print('[U]pdate client')
+    print('[L]ist client')
     print('[D]elete client')
+    print('[S]earch client')
 
 def _get_client_name():
     return input('What is the client name?')
@@ -56,10 +67,22 @@ if __name__ == '__main__':
         client_name = _get_client_name()
         create_client(client_name)
         list_clients()
+    elif commad == 'L':
+        list_clients()
     elif commad == 'D':
         client_name = _get_client_name()
         delete_client(client_name)
         list_clients()
+    elif commad == 'S':
+        client_name = _get_client_name()
+        found = search_client(client_name)
+
+        if found:
+            print('The client is in the client\'s list')
+        else:
+            print('The client:{} is not in our client\'s list'.format(client_name))
+
+
     elif commad == 'U':
         client_name = _get_client_name()
         update_client_name = input('What is the update client name?')
