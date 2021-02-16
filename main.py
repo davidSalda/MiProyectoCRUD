bd0216496dac187fdff8a449dc5e3bb13b02a9f4
David s <dadiortiz403@gmail.com>
2021-02-15 23:20:23 -0600

    Fin de la parte 03 'Estructuras de datos'

--- main.py
+++ main.py
@@ -1,47 +1,70 @@
+import csv
+import os
 import sys
-clients = 'pablo,ricardo,'
 
-def create_client(client_name):
+
+
+clients = []
+CLIENT_TABLE = '.clients.csv'
+CLIENT_SCHEMA = ['name', 'company', 'email', 'position']
+
+def _initialize_clients_from_storage():
+    with open(CLIENT_TABLE, mode='r') as f:
+        reader = csv.DictReader(f, fieldnames=CLIENT_SCHEMA)
+
+        for row in reader:
+            clients.append(row)
+
+def _save_clients_to_storage():
+    tmp_table_name = '{}.tmp'.format(CLIENT_TABLE)
+    with open(tmp_table_name, mode='w') as f:
+        writer = csv.DictWriter(f, fieldnames=CLIENT_SCHEMA)
+        writer.writerows(clients)
+
+        os.remove(CLIENT_TABLE)
+        os.rename(tmp_table_name, CLIENT_TABLE)
+
+def create_client(client):
     global clients
 
-    clients += client_name
-    _add_comma()
+    if client not in clients:
+        clients.append(client)
+    else:
+        print('Client already in client\'s list')
 
 
 def list_clients():
-    global clients
+    for idx, client in enumerate(clients):
+        print('{uid} | {name} | {company} | {email} | {position}'.format(
+            uid=idx,
+            name=client['name'],
+            company=client['company'],
+            email=client['email'],
+            position=client['position'],))
 
-    print(clients)
-
-def update_client(client_name, update_client_name):
+def update_client(client_name, updated_name):
     global clients
 
     if client_name in clients:
-        clients = clients.replace(client_name + ',', update_client_name + ',')
+        index = clients.index(client_name)
+        clients[index] = updated_name
     else:
-        print('Clients is not in clients list')
+        print('Clients is not in client\'s list')
 
 def delete_client(client_name):
     global clients
 
     if client_name in clients:
-        clients = clients.replace(client_name + ',', '')
+        clients.remove(client_name)
     else:
-        print('Client is not in clients list')
+        print('Client is not in client\'s list')
 
 def search_client(client_name):
-    clients_list = clients.split(',')
-
-    for client in clients_list:
+    for client in clients:
         if client != client_name:
             continue
         else:
             return True
-
-def _add_comma():
-    global clients
-
-    clients += ","
 
 
 def _print_welcome():
@@ -53,6 +76,14 @@ def _print_welcome():
     print('[L]ist client')
     print('[D]elete client')
     print('[S]earch client')
+
+def _get_client_field(field_name):
+    field = None
+
+    while not field:
+        field = input('What is the client {}?'.format(field_name))
+    return field
+
 
 def _get_client_name():
     client_name = None
@@ -71,21 +102,26 @@ def _get_client_name():
 
 
 if __name__ == '__main__':
+    _initialize_clients_from_storage()
     _print_welcome()
 
     commad = input()
-    commad = commad.upper()  #Sirve para que al pedir llos valores sean em mayuscula
+    commad = commad.upper()  #Sirve para que al pedir los valores sean en mayuscula
 
     if commad == 'C':
-        client_name = _get_client_name()
-        create_client(client_name)
-        list_clients()
+        client = {
+            'name' : _get_client_field('name'),
+            'company' : _get_client_field('company'),
+            'email': _get_client_field('email'),
+            'position': _get_client_field('position'),
+        }
+        create_client(client)
+
     elif commad == 'L':
         list_clients()
     elif commad == 'D':
         client_name = _get_client_name()
         delete_client(client_name)
-        list_clients()
     elif commad == 'S':
         client_name = _get_client_name()
         found = search_client(client_name)
@@ -100,7 +136,6 @@ if __name__ == '__main__':
         client_name = _get_client_name()
         update_client_name = input('What is the update client name?')
         update_client(client_name, update_client_name)
-        list_clients()
         pass
     else:
         print('Invalid command')
